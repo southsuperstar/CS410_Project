7f1f9e01d2a755a03fd8c4d9cac6d28a4e9255f1
southsuperstar <spencer.reid11@gmail.com>
2018-11-25 12:36:27 -0800

    update

--- parse_dataset/parse_dataset.py
+++ parse_dataset/parse_dataset.py
@@ -3,6 +3,18 @@ import glob
 
 
 '''
+    Columns to be removed from the original yelp dataset.
+'''
+
+business_labels_drop = ['hours', 'is_open', 'latitude', 'longitude', 'neighborhood']
+review_labels_drop = ['cool', 'funny']
+user_labels_drop = ['friends', 'useful', 'funny', 'cool', 'fans', 'compliment_hot', 'compliment_more', 'compliment_profile',
+                    'compliment_cute', 'compliment_list', 'compliment_note', 'compliment_plain', 'compliment_cool',
+                    'compliment_funny', 'compliment_writer', 'compliment_photos']
+merge_labels_drop = ['business_id', 'review_id', 'user_id']
+
+
+'''
     Parse the business json log from yelp dataset.
     Filter in the businesses fit below criteria:
         1. It is a restaurant.
@@ -12,7 +24,7 @@ import glob
     After filtering the data, convert to csv file.
 '''
 
-business_dataset_name = './dataset/yelp_academic_dataset_business.json'
+business_dataset_name = '../yelp_dataset/yelp_academic_dataset_business.json'
 business_df = pd.read_json(business_dataset_name, lines=True)
 business_df['city'] = business_df['city'].str.upper()
 business_df = business_df.dropna(subset=['categories'])
@@ -20,6 +32,8 @@ business_df = business_df[business_df['categories'].str.contains('Restaurants')]
 business_df = business_df[business_df['categories'].str.contains('Chinese')]
 business_df = business_df[(business_df.review_count >= 100) & (business_df.is_open == 1) & (business_df.stars >= 3.5)]
 dseries = business_df['business_id'].tolist()
+business_df.drop(business_labels_drop, axis=1, inplace=True)
+business_df = business_df.rename(index=str, columns={'stars':'business_avg_star', 'name':'business_name', 'review_count':'business_review_count'})
 business_df.to_csv('business_chinese_restaurants.csv', encoding='utf-8')
 # df = df.groupby('city')['name'].nunique().sort_values(ascending=True)
 
@@ -33,7 +47,7 @@ business_df.to_csv('business_chinese_restaurants.csv', encoding='utf-8')
 '''
 
 for i in range(6):
-    review_dataset_name = './dataset/yelp_academic_dataset_review0' + str(i)
+    review_dataset_name = '../yelp_dataset/yelp_academic_dataset_review0' + str(i)
     review_df = pd.read_json(review_dataset_name, lines=True)
     review_df = review_df.dropna()
     review_df = review_df[review_df['business_id'].isin(dseries)]
@@ -47,7 +61,9 @@ for file_ in review_subfiles:
     list_.append(df_)
 all_review_df = pd.concat(list_)
 all_review_df = all_review_df.dropna()
-all_review_df.to_csv('review_chinese_restaurants_all.csv', encoding='utf-8')
+all_review_df.drop(review_labels_drop, axis=1, inplace=True)
+all_review_df = all_review_df.rename(index=str, columns={'stars':'review_from_user'})
+all_review_df.to_csv('rev_chinese_restaurants_all.csv', encoding='utf-8')
 
 
 '''
@@ -57,10 +73,12 @@ all_review_df.to_csv('review_chinese_restaurants_all.csv', encoding='utf-8')
 '''
 
 users = list(set(all_review_df['user_id'].tolist()))
-user_dataset_name = './yelp_academic_dataset_user.json'
+user_dataset_name = '../yelp_dataset/yelp_academic_dataset_user.json'
 user_df = pd.read_json(user_dataset_name, lines=True)
 user_df = user_df.dropna()
 user_df = user_df[user_df['user_id'].isin(users)]
+user_df.drop(user_labels_drop, axis=1, inplace=True)
+user_df = user_df.rename(index=str, columns={'name':'user_name', 'review_count':'user_total_review_count'})
 user_df.to_csv('users_chinese_restaurants.csv', encoding='utf-8')
 
 
@@ -69,6 +87,9 @@ user_df.to_csv('users_chinese_restaurants.csv', encoding='utf-8')
     Save the merged file to csv.
 '''
 
-results = review_df.merge(business_df, on='business_id')
+results = all_review_df.merge(business_df, on='business_id')
 results = results.merge(user_df, on='user_id')
-results.to_csv('merged_user_review_business_results.csv', encoding='utf-8')
+results['business_and_id'] = results['business_name'].str.cat(results['business_id'], sep='_')
+results['user_and_id'] = results['user_name'].str.cat(results['user_id'], sep=',')
+results.drop(merge_labels_drop, axis=1, inplace=True)
+results.to_csv('merged_user_rev_business_results.csv', encoding='utf-8')
